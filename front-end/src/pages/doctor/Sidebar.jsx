d55b655397ab0b8893e4a55b8d8927cf8a1f55fb
Tuấn <tuan.nguyen62@hcmut.edu.vn>
2025-05-13 13:22:23 +0700

    Update Sidebar.jsx
    Style lai Sidebar

--- front-end/src/pages/doctor/Sidebar.jsx
+++ front-end/src/pages/doctor/Sidebar.jsx
@@ -4,8 +4,8 @@ import BimecLogo from '../../assets/icon/Bimec_logo.png';
 
 function Sidebar() {
   return (
-    <aside className="w-48 bg-white border-r border-gray-200 flex flex-col">
-      <div className="flex items-center gap-2 px-6 py-6 border-b border-gray-200">
+    <aside className="w-[200px] bg-white border-r border-gray-200 flex flex-col">
+      <div className="flex items-center gap-2 px-6 py-6 border-gray-200">
         <img
           alt="BIMEC logo"
           className="w-6 h-6"
@@ -22,7 +22,7 @@ function Sidebar() {
           to="/doctor/appointments"
           className={({ isActive }) =>
             isActive
-              ? "flex items-center gap-3 px-4 py-3 rounded-xl text-[#2F4F2F] bg-[#E6F6FF] font-semibold select-none"
+              ? "flex items-center gap-3 px-4 py-3 rounded-xl text-[#698C55] bg-[#E6F6FF] backdrop-blur-md font-semibold select-none"
               : "flex items-center gap-3 px-4 py-3 rounded-xl text-gray-500 hover:text-gray-700 hover:bg-gray-100 transition"
           }
         >
@@ -33,7 +33,7 @@ function Sidebar() {
           to="/doctor/patients"
           className={({ isActive }) =>
             isActive
-              ? "flex items-center gap-3 px-4 py-3 rounded-xl text-[#2F4F2F] bg-[#E6F6FF] font-semibold select-none"
+              ? "flex items-center gap-3 px-4 py-3 rounded-xl text-[#698C55] bg-[#E6F6FF] backdrop-blur-md font-semibold select-none"
               : "flex items-center gap-3 px-4 py-3 rounded-xl text-gray-500 hover:text-gray-700 hover:bg-gray-100 transition"
           }
         >
@@ -44,7 +44,7 @@ function Sidebar() {
           to="/doctor/schedule"
           className={({ isActive }) =>
             isActive
-              ? "flex items-center gap-3 px-4 py-3 rounded-xl text-[#2F4F2F] bg-[#E6F6FF] font-semibold select-none"
+              ? "flex items-center gap-3 px-4 py-3 rounded-xl text-[#698C55] bg-[#E6F6FF] backdrop-blur-md font-semibold select-none"
               : "flex items-center gap-3 px-4 py-3 rounded-xl text-gray-500 hover:text-gray-700 hover:bg-gray-100 transition"
           }
         >
