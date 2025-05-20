351883f76a310aba8194a19237bc7a00faac0fdb
khiemttruong <khiem.t.truong@tgl-sol.com>
2025-05-21 03:17:35 +0700

    fix: MONGODB_URI

--- back-end/db.js
+++ back-end/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = async function connection() {
     try {
-        await mongoose.connect(process.env.DB);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("MongoDB connected successfully");
     } catch (error) {
         console.error("MongoDB connection error:", error.message);
